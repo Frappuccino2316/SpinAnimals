a9112caba3c27db4e66a359ed98a7e9f60c086d8
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-10 20:15:10 +0900

    Initial Commit

---
+++ StupidSpinAll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StupidSpinAll
+//
+//  Created by 髙木文弘 on 2021/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StupidSpinAll/StupidSpinAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  StupidSpinAllApp.swift
+//  StupidSpinAll
+//
+//  Created by 髙木文弘 on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct StupidSpinAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
